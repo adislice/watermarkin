d92ad6aed624ec9158e7e030c18c3b6ed5b34681
R. Adi Permana <radipermana29@gmail.com>
2024-08-27 14:30:49 +0700

    Add and configure vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import HomePage from '@/pages/HomePage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomePage }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col p-5">
+    <h1 class="">Watermark Dokumen</h1>
+
+  </div>
+</template>
